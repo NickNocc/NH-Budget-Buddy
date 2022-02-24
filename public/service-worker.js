564223fd869804b7c7a29459d85c7cab902076eb
NickNocc <haretakis4@gmail.com>
2022-02-24 14:59:09 -0800

    a

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,22 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-const FILES_TO_CACHE = [
-    './public/index.html',
-    './public/css/styles.css',
-    './public/js/index.js'
-];
 
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url);
-            }
-            console.log('file is not cached, fetching : ' + e.request.url);
-            return fetch(e.request)
-        })
-    )
-});
+const FILES_TO_CACHE = [
+    './index.html',
+    './manifest.json',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
+];
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
@@ -45,4 +43,19 @@ self.addEventListener('activate', function(e) {
             }));
         })
     );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request;
+            } else {
+            console.log('File is not cached, fetching : ' + e.request.url);
+            return fetch(e.request);
+            }
+        })
+    )
 });
